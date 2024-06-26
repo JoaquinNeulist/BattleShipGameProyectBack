ea138b11623c40af5d81f9bfbe241b6ce3ad7b18
JoaquinNeulist <joaquinneulist@gmail.com>
2024-06-26 13:35:35 -0300

    intento de creacion de tiros

--- src/main/java/com/Battleship/Game/controllers/ShootController.java
+++ src/main/java/com/Battleship/Game/controllers/ShootController.java
@@ -1,9 +1,28 @@
 package com.Battleship.Game.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.Battleship.Game.dtos.ShootRequest;
+import com.Battleship.Game.models.Board;
+import com.Battleship.Game.repositories.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authorization.method.AuthorizeReturnObject;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/shoot")
 public class ShootController {
+
+    @Autowired
+    private BoardRepository boardRepository;
+
+    @PostMapping("/{boardId}")
+    public ResponseEntity<?> shoot(@PathVariable Long boardId, @RequestBody ShootRequest shootRequest, Authentication authentication) {
+        Board board = boardRepository.findById(boardId).orElse(null);
+        if (board == null) {
+            return new ResponseEntity<>("Board not found", HttpStatus.FORBIDDEN);
+        }
+        return new ResponseEntity<>(shootRequest, HttpStatus.OK);
+    }
 }
---
+++ src/main/java/com/Battleship/Game/dtos/ShootRequest.java
@@ -0,0 +1,4 @@
+package com.Battleship.Game.dtos;
+
+public record ShootRequest(int cordX, int cordY) {
+}
