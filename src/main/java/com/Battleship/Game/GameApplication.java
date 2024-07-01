d30578fccfa52cb62683ab9655629258d421204e
Mayco Dominguez <maycodominguez123@gmail.com>
2024-07-01 12:18:54 -0300

    cambio de direcciones

--- src/main/java/com/Battleship/Game/GameApplication.java
+++ src/main/java/com/Battleship/Game/GameApplication.java
@@ -29,34 +29,34 @@ public class GameApplication {
 			ShipRepository shipRepository
 	){
 		return args -> {
-			Account account1 = new Account("Test@mail.com", "joaco", "neulist", "Jota", passwordEncoder.encode("123"), 15);
-			accountRepository.save(account1);
-			System.out.println(account1);
-			Account account2 = new Account("Test@mail2.com", "Agus", "Russo", "AgusR", passwordEncoder.encode("123"), 10);
-			account2.setAdmin(true);
-            accountRepository.save(account2);
-			Account account3 = new Account("Test@mail3.com", "Mayco", "Dominguez", "Mayk", passwordEncoder.encode("123"), 9);
-			accountRepository.save(account3);
-			Account account4 = new Account("Test@mail4.com", "Chris", "Vega", "Kryzpo", passwordEncoder.encode("123"), 15);
-			accountRepository.save(account4);
-			// Agregar más usuarios para pruebas
-			Account account5 = new Account("Test@mail5.com", "Edmundo", "Ruveda", "Edmun", passwordEncoder.encode("123"), 4);
-			accountRepository.save(account5);
-
-			Account account6 = new Account("Test@mail6.com", "Mariela", "Morsucci", "Mari", passwordEncoder.encode("123"), 5);
-			accountRepository.save(account6);
-
-			Account account7 = new Account("Test@mail7.com", "Diego", "Vallejo", "Dieguito", passwordEncoder.encode("123"), 6);
-			accountRepository.save(account7);
-
-			Account account8 = new Account("Test@mail8.com", "Pedro", "Sanabria", "Pedrito", passwordEncoder.encode("123"), 7);
-			accountRepository.save(account8);
-
-			Account account9 = new Account("Test@mail9.com", "Jelena", "Palavecino", "Jele", passwordEncoder.encode("123"), 8);
-			accountRepository.save(account9);
-
-			Account account10 = new Account("Test@mail10.com", "Flor", "Ayala", "Flor", passwordEncoder.encode("123"), 9);
-			accountRepository.save(account10);
+//			Account account1 = new Account("Test@mail.com", "joaco", "neulist", "Jota", passwordEncoder.encode("123"), 15);
+//			accountRepository.save(account1);
+//			System.out.println(account1);
+//			Account account2 = new Account("Test@mail2.com", "Agus", "Russo", "AgusR", passwordEncoder.encode("123"), 10);
+//			account2.setAdmin(true);
+//            accountRepository.save(account2);
+//			Account account3 = new Account("Test@mail3.com", "Mayco", "Dominguez", "Mayk", passwordEncoder.encode("123"), 9);
+//			accountRepository.save(account3);
+//			Account account4 = new Account("Test@mail4.com", "Chris", "Vega", "Kryzpo", passwordEncoder.encode("123"), 15);
+//			accountRepository.save(account4);
+//			// Agregar más usuarios para pruebas
+//			Account account5 = new Account("Test@mail5.com", "Edmundo", "Ruveda", "Edmun", passwordEncoder.encode("123"), 4);
+//			accountRepository.save(account5);
+//
+//			Account account6 = new Account("Test@mail6.com", "Mariela", "Morsucci", "Mari", passwordEncoder.encode("123"), 5);
+//			accountRepository.save(account6);
+//
+//			Account account7 = new Account("Test@mail7.com", "Diego", "Vallejo", "Dieguito", passwordEncoder.encode("123"), 6);
+//			accountRepository.save(account7);
+//
+//			Account account8 = new Account("Test@mail8.com", "Pedro", "Sanabria", "Pedrito", passwordEncoder.encode("123"), 7);
+//			accountRepository.save(account8);
+//
+//			Account account9 = new Account("Test@mail9.com", "Jelena", "Palavecino", "Jele", passwordEncoder.encode("123"), 8);
+//			accountRepository.save(account9);
+//
+//			Account account10 = new Account("Test@mail10.com", "Flor", "Ayala", "Flor", passwordEncoder.encode("123"), 9);
+//			accountRepository.save(account10);
 		};
 	}
 }
--- src/main/java/com/Battleship/Game/configurations/CorsConfig.java
+++ src/main/java/com/Battleship/Game/configurations/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173","http://localhost:8080"));
+        configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE"));
         configuration.setAllowedHeaders(List.of("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
