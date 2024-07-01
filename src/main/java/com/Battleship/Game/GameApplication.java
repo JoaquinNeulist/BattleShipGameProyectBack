2cbd09af35ed282f074240e695abb70229935bc1
Mayco Dominguez <maycodominguez123@gmail.com>
2024-07-01 02:17:00 -0300

    properties

--- src/main/java/com/Battleship/Game/GameApplication.java
+++ src/main/java/com/Battleship/Game/GameApplication.java
@@ -31,7 +31,6 @@ public class GameApplication {
 		return args -> {
 			Account account1 = new Account("Test@mail.com", "joaco", "neulist", "Jota", passwordEncoder.encode("123"), 15);
 			accountRepository.save(account1);
-			account1.setAdmin(true);
 			System.out.println(account1);
 			Account account2 = new Account("Test@mail2.com", "Agus", "Russo", "AgusR", passwordEncoder.encode("123"), 10);
             accountRepository.save(account2);
@@ -40,22 +39,22 @@ public class GameApplication {
 			Account account4 = new Account("Test@mail4.com", "Chris", "Vega", "Kryzpo", passwordEncoder.encode("123"), 15);
 			accountRepository.save(account4);
 			// Agregar más usuarios para pruebas
-			Account account5 = new Account("Test@mail5.com", "Usuario5", "Apellido5", "Usuario5", passwordEncoder.encode("123"), 4);
+			Account account5 = new Account("Test@mail5.com", "Edmundo", "Ruveda", "Edmun", passwordEncoder.encode("123"), 4);
 			accountRepository.save(account5);
 
-			Account account6 = new Account("Test@mail6.com", "Usuario6", "Apellido6", "Usuario6", passwordEncoder.encode("123"), 5);
+			Account account6 = new Account("Test@mail6.com", "Mariela", "Morsucci", "Mari", passwordEncoder.encode("123"), 5);
 			accountRepository.save(account6);
 
-			Account account7 = new Account("Test@mail7.com", "Usuario7", "Apellido7", "Usuario7", passwordEncoder.encode("123"), 6);
+			Account account7 = new Account("Test@mail7.com", "Diego", "Vallejo", "Dieguito", passwordEncoder.encode("123"), 6);
 			accountRepository.save(account7);
 
-			Account account8 = new Account("Test@mail8.com", "Usuario8", "Apellido8", "Usuario8", passwordEncoder.encode("123"), 7);
+			Account account8 = new Account("Test@mail8.com", "Pedro", "Sanabria", "Pedrito", passwordEncoder.encode("123"), 7);
 			accountRepository.save(account8);
 
-			Account account9 = new Account("Test@mail9.com", "Usuario9", "Apellido9", "Usuario9", passwordEncoder.encode("123"), 8);
+			Account account9 = new Account("Test@mail9.com", "Jelena", "Palavecino", "Jele", passwordEncoder.encode("123"), 8);
 			accountRepository.save(account9);
 
-			Account account10 = new Account("Test@mail10.com", "Usuario10", "Apellido10", "Usuario10", passwordEncoder.encode("123"), 9);
+			Account account10 = new Account("Test@mail10.com", "Flor", "Ayala", "Flor", passwordEncoder.encode("123"), 9);
 			accountRepository.save(account10);
 		};
 	}
